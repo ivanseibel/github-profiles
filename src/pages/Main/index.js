77300e55fbba20243dea4c4158daa7c62c927aef
Ivan Seibel <ivanseibel@gmail.com>
2020-04-04 16:03:14 -0300

    React Stack Navigation installed

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <View>
+      <Text>Main</Text>
+    </View>
+  );
+}
